0b4f1c5b5be8db5223c97106ea2c760b22ab8321
Jake Son <sonjeabin@gmail.com>
2024-10-26 16:11:18 +0900

    refactor: apply typescript

---
+++ src/list.ts
@@ -0,0 +1,109 @@
+import GetAllDetails = chrome.cookies.GetAllDetails;
+import Cookie = chrome.cookies.Cookie;
+import DataTable, {Api} from 'datatables.net-dt';
+
+export function setupList() {
+  const $searchForm = document.getElementById("search-form") as HTMLFormElement;
+  const domainInput = document.getElementById("domain-input") as HTMLInputElement;
+  const nameInput = document.getElementById("name-input") as HTMLInputElement;
+  const $searchResultTable = document.getElementById("search-result-table") as HTMLElement;
+  const $searchCheckbox = document.getElementById("search-checkbox") as HTMLInputElement;
+
+  let isDecodeEnabled = true;
+
+  (async function initPopupWindow() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tab?.url) {
+      try {
+        domainInput.value = new URL(tab.url).toString();
+        await searchCookies(domainInput.value, nameInput.value);
+      } catch {}
+    }
+
+    nameInput.focus();
+  })();
+
+  $searchForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    clearList();
+    await searchCookies(domainInput.value, nameInput.value);
+  });
+
+  $searchResultTable.addEventListener("click", (event) => {
+    const $CopyBtn: any = (event.target as HTMLElement)?.closest(".value-copy");
+    if ($CopyBtn) {
+      navigator.clipboard.writeText($CopyBtn.dataset.value).then(() => {
+        // alert("Copied", $CopyBtn.dataset.value);
+      });
+    }
+  });
+
+  $searchCheckbox.addEventListener("change", async () => {
+    isDecodeEnabled = $searchCheckbox.checked;
+    await searchCookies(domainInput.value, nameInput.value);
+  });
+
+  async function searchCookies(domain: string, name: string) {
+    if (!domain && !name) return "No domain or name provided";
+    try {
+      const searchObject: GetAllDetails = {};
+
+      if (domain) {
+        searchObject.url = domain;
+      }
+
+      const cookies = await chrome.cookies.getAll(searchObject);
+      const filtered  = name ? cookies.filter(x => x.name.includes(name)) : cookies;
+      const decoded  = isDecodeEnabled ? filtered.map(x => ({
+        ...x,
+        value: decodeURIComponent(x.value)
+      })) : filtered;
+      const orderedCookies = decoded.sort((a, b) => a.name.localeCompare(b.name));
+
+      return drawCookieList(orderedCookies);
+    } catch (error: any) {
+      return `Unexpected error: ${error.message}`;
+    }
+  }
+
+  let datatable: Api;
+  async function drawCookieList(cookies: Cookie[]) {
+    if (datatable) {
+      datatable.destroy();
+    }
+    datatable = new DataTable("#search-result-table", {
+      data: cookies,
+      paging: false,
+      searching: false,
+      ordering: false,
+      scrollX: true,
+      scrollY: '400',
+      scrollCollapse: true,
+      columns: [
+        {
+          data: "name",
+        },
+        {
+          data: "domain",
+        },
+        {
+          render: (_data:any, _type:any, row:any) => {
+            return `<button class="table-tool-btn value-copy" data-value="${row.value}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512"><path d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z"/></svg></button>`;
+          },
+        },
+        {
+          data: "value",
+        },
+        {
+          data: "path",
+        },
+      ],
+    });
+  }
+
+  function clearList() {
+    $searchResultTable.innerHTML = ""; // Clear all child elements
+  }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,44 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {setupList} from "./list.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <div id="header">
+    <!-- <h3>bookmark</h3> -->
+    <!-- <div id="bookmark-list"></div> -->
   </div>
+  <section id="search-section">
+    <h3>Search</h3>
+    <label for="search-checkbox">Decode Value</label>
+    <input type="checkbox" id="search-checkbox" checked />
+    <form id="search-form">
+      <label for="domain-input">Domain</label>
+      <input type="text" id="domain-input" />
+      <label for="name-input">Name</label>
+      <input type="text" id="name-input" />
+      <button type="submit" class="submit-btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 512 512"
+        >
+          <path
+            d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6 .1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+          />
+        </svg>
+      </button>
+    </form>
+  </section>
+  <section id="search-result">
+    <table
+      id="search-result-table"
+      class="display compact order-column stripe"
+      style="width: 100%"
+    ></table>
+  </section>
+  <!-- <button id="save-btn">Save Cookies</button>
+  <button id="remove-btn">Remove Cookies</button>
+  <button id="clear-btn">Clear Cookies</button> -->
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupList();
